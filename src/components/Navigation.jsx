c00ad15c0d027606505f9152117b339b00415a5c
Zakariasisu5 <zakariasisu5@gmail.com>
2025-11-07 01:10:21 +0000

    commit

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,7 @@
 import { Link, useLocation } from 'react-router-dom'
 import { useState } from 'react'
 import './Navigation.css'
-import Logo from "../assets/logo.jpg"
+import Logo from "../assets/8nova1.png"
 import { FaHome, FaUsers, FaBullseye, FaFolderOpen, FaEnvelope, FaBars, FaTimes } from 'react-icons/fa';
 
 const Navigation = () => {
